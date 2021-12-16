0e6c945abb8d9fab2a4de77d2975b260a5a7eb72
marcobrito <marco.brito@solides.com.br>
2021-12-16 13:04:17 -0300

    Retirado paginas sem uso

--- src/components/Menu/index.js
+++ src/components/Menu/index.js
@@ -3,35 +3,23 @@ import {
   InfoCircleOutlined,
   PoundOutlined,
   MailOutlined,
-  HomeOutlined
 } from "@ant-design/icons";
-import { SiYourtraveldottv } from "react-icons/si";
-import { useRouter } from 'next/router'
 import Link from 'next/link'
 
 const { SubMenu } = Menu;
 
 export const MenuResponsive = () => {
-  const router = useRouter()
-
   return (
     <Menu
         style={{ width: '100%' }}
         mode='horizontal'
         theme='dark'
       >
-        <Menu.Item key="home" icon={<HomeOutlined />} onClick={() => router.push('/')}>
-          Inicio
-        </Menu.Item>
-        <Menu.Item key="budget" icon={<PoundOutlined />} onClick={() => router.push('/budget')}>
+        <Menu.Item key="budget" icon={<PoundOutlined />}>
           Orcamento
-        </Menu.Item>
-        <Menu.Item key="about" icon={<InfoCircleOutlined />}>
-          Sobre
         </Menu.Item>
         <SubMenu key="contact" icon={<MailOutlined />} title="Contato">
           <Menu.Item key="contact1"><Link href="https://wa.me/5538999299640"><a target="_blank">Whatsapp</a></Link></Menu.Item>
-          <Menu.Item key="contact2">Email</Menu.Item>
           <Menu.Item key="contact3"><Link href="https://instagram.com/soborafaci?utm_medium=copy_link"><a target="_blank">Instagram</a></Link></Menu.Item>
         </SubMenu>
       </Menu>
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,20 +1,115 @@
 import Head from "next/head";
+import axios from 'axios'
+import React, { useContext } from 'react';
+import { format, parseISO } from 'date-fns'
+import { Form, Input, Button, DatePicker,  Switch, message } from 'antd';
 
-import { Carrosel } from "../components/Carrosel";
-import styles from "../styles/Carrosel/Carrosel.module.scss";
+import { IsMobileContext } from '../context/IsMobileContext';
+import styles from '../styles/Pages/Budget.module.scss'
 
-const Home = () => {
-  return (
-    <>
-      <Head>
-        <title>Home | Só Bora</title>
-      </Head>
+export default function Home() {
+  const { isMobile } = useContext(IsMobileContext)
+  const [form] = Form.useForm();
 
-      <div className={styles.carrosel}>
-        <Carrosel />
-      </div>
-    </>
-  );
+  const sendMail = async (form) => {  
+    const dateGoing = form.datas[0]
+    const dateReturn = form.datas[1]
+    
+    const parsedDateGoing = parseISO(dateGoing)
+    const parsedDateReturn = parseISO(dateReturn)
+
+    const formattedDateGoing = format(
+      parsedDateGoing,
+      "'Ida:' dd 'de' MMMM 'de' YYYY"
+    )
+
+    const formattedDateReturn = format(
+      parsedDateReturn,
+      "'Ida:' dd 'de' MMMM 'de' YYYY"
+    )
+  
+    const mail = {
+    service_id: 'client_contact',
+    template_id: 'CONTACT_FORM',
+    user_id: 'user_l59sCh4Xn57hN4RUhJoJT',
+    template_params: {
+      'nome': form.nome,
+      'origem': form.origem,
+      'destino': form.destino,
+      'ida': formattedDateGoing,
+      'volta': formattedDateReturn,
+      'email': form.email,
+      'observacoes': form.observacoes,
+      'aceitaSemelhante': form.aceitaSemelhante ? 'sim' : 'nao'
+    }
 };
 
-export default Home;
+  await axios({
+    method: 'post',
+    url: 'https://api.emailjs.com/api/v1.0/email/send',
+    data: mail,
+    headers: {
+        'content-type': 'application/json'
+    }
+  }).then(res => {
+  message.success('Enviado com sucesso');
+  }).catch(err => {
+  message.error('Erro no envio, tente novamente ou entre em contato');
+  })
+
+  }
+
+  return (
+    <><Head>
+    <title>Orcamento | Só Bora</title>
+  </Head>
+  
+  <div className={styles.container}>
+    <Form
+      form={form}
+      layout="vertical"
+      requiredMark
+      onFinish={sendMail}
+      style={isMobile ? {width: '80%'} : {width: '40%'}}
+    >
+      <Form.Item label="Seu nome completo" required name='nome'>
+        <Input />
+      </Form.Item>
+      <Form.Item
+        label="Origem de partida"
+        name='origem'
+      ><Input /></Form.Item>
+      <Form.Item
+        label="Destino desejado"
+        required
+        name='destino'
+      >
+        <Input />
+      </Form.Item>
+      <Form.Item label='Periodo que deseja viajar' required name='datas'>
+      <DatePicker.RangePicker style={{ width: '70%' }} />
+      </Form.Item>
+
+    <Form.Item
+        label="Seu melhor email"
+        required
+        name='email'
+      >
+        <Input />
+      </Form.Item>
+    <Form.Item
+        label="Observacoes extras"
+        required
+        name='observacoes'
+      >
+        <Input />
+      </Form.Item>
+      <Form.Item label="Gostaria de receber orcamento de viagens semelhantes ?" valuePropName="checked" name="aceitaSemelhante">
+        <Switch />
+      </Form.Item>
+        <Button type="primary" size='large' style={{width: "100%"}} htmlType='submit'>Enviar</Button>
+    </Form>
+    </div>
+  </>
+  );
+};
