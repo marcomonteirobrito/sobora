2528c7e2762a8bdc350ec1dc5266aaae6edb6374
MarcoBrito <marco.brito@solides.com.br>
2022-01-14 13:45:01 -0300

    Retirada do footer

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -10,7 +10,7 @@ function MyApp({ Component, pageProps }) {
     <IsMobileProvider>
       <MenuResponsive />
       <Component {...pageProps} />
-      <Footer />
+      {/* <Footer /> */}
     </IsMobileProvider>
   );
 }
